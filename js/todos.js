82eb9519331b74028951eb613d081708a6019dc0
Josh <zylo.codes@gmail.com>
2021-03-09 22:10:27 -0600

    Implements IndexedDB!

--- js/todos.js
+++ js/todos.js
@@ -1,13 +1,37 @@
 document.addEventListener('DOMContentLoaded', (e) => {
 
+  let db
+  const request = window.indexedDB.open('TodoList', 1)
+
+  request.onerror = e => console.log(e.target.errorCode)
+
+  request.onupgradeneeded = e => {
+    const db = e.target.result
+    db.createObjectStore('todos', { keyPath: 'id', autoIncrement: true })
+  }
+
+  request.onsuccess = e => {
+    // set global db to opened DB
+    db = e.target.result
+    console.log(`Successfully opened ${db.name}`)
+    getTodos()
+  }
+
+  const getTodosTransactionStore = () => {
+    const transaction = db.transaction(['todos'], 'readwrite')
+    return transaction.objectStore('todos')
+  }
+
   const form = document.getElementById('todo-form')
   const newTodoInput = document.querySelector('input.new-item')
   const todoListSpan = document.querySelector('.todo-container')
 
   const getTodos = () => {
-    // fetch('/api/todos')
-    //   .then(response => response.json())
-    //   .then(todos => renderTodoList(todos))
+    const store = getTodosTransactionStore()
+    const getAllRequest = store.getAll()
+    getAllRequest.onsuccess = e => {
+      renderTodoList(getAllRequest.result)
+    }
   }
 
   const renderTodoList = todos => {
@@ -26,48 +50,39 @@ document.addEventListener('DOMContentLoaded', (e) => {
   form.addEventListener('submit', e => {
     e.preventDefault()
     const text = newTodoInput.value
-    // fetch('/api/todos', {
-    //   method: 'POST',
-    //   body: JSON.stringify({ text }),
-    //   headers: {
-    //     'Content-Type': 'application/json'
-    //   }
-    // })
-    //   .then(getTodos)
-    //   .catch(err => console.error(err))
+    const store = getTodosTransactionStore()
+    store.add({
+      text,
+      complete: false
+    })
+    newTodoInput.value = ''
+    getTodos()
   })
 
-
   const deleteTodo = id => {
-    // fetch(`/api/todos/${id}`, {
-    //   method: 'DELETE'
-    // })
-    //   .then(getTodos)
-    //   .catch(err => console.error(err))
+    const store = getTodosTransactionStore()
+    store.delete(parseInt(id))
+    getTodos()
   }
 
   const updateTodo = newTodo => {
-    // fetch(`/api/todos/${newTodo.id}`, {
-    //   method: 'PUT',
-    //   headers: {
-    //     'Content-Type': 'application/json'
-    //   },
-    //   body: JSON.stringify(newTodo)
-    // })
-    //   .then(getTodos)
-    //   .catch(err => console.error(err))
+    const store = getTodosTransactionStore()
+    store.put(newTodo)
+    getTodos()
   }
 
   todoListSpan.addEventListener('click', e => {
     const target = e.target
-    const id = target.getAttribute('data-id')
+    const id = parseInt(target.getAttribute('data-id'))
     if (target.matches('.delete')) {
       deleteTodo(id)
     } else if (target.matches('.complete')) {
       const complete = JSON.parse(target.getAttribute('data-complete'))
+      const text = e.target.previousElementSibling.previousElementSibling.previousElementSibling.innerText
       const newTodo = {
         id,
-        complete: !complete
+        complete: !complete,
+        text,
       }
       updateTodo(newTodo)
     } else if (target.matches('span')) {
@@ -80,9 +95,11 @@ document.addEventListener('DOMContentLoaded', (e) => {
 
   todoListSpan.addEventListener('keyup', e => {
     if (e.keyCode === 13) {
+      const complete = e.target.nextElementSibling.nextElementSibling.getAttribute('data-complete') === 'true'
       const newTodo = {
-        id: e.target.getAttribute('data-id'),
-        text: e.target.value
+        id: parseInt(e.target.getAttribute('data-id')),
+        text: e.target.value,
+        complete,
       }      
       updateTodo(newTodo)
     }
@@ -96,6 +113,4 @@ document.addEventListener('DOMContentLoaded', (e) => {
       e.target.style.display = "none"
     }
   }, true)
-
-  // getTodos()
 });
